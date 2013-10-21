0f2933715912584a1ae8512198d61cb6d6f6026d
Technus <daniel112092@gmail.com>
2013-10-21 22:51:13 +0200

    Update mainframe main.lua

--- mainframe main.lua
+++ mainframe main.lua
@@ -1,4 +1,10 @@
---List of Fuctions
-function addIO()
+--List of Fuctions for redstone interactions
+
+--adds ON-OFF redstone (analog) and togglable by impulse redstone IO's restone to database
+function addIO(id,name,descr,method,dirval)--type
+  
 end
 
+--init phase
+
+os.run("restoneDB")
