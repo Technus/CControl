f56cb3072dca0ea671c12a37ae0d9d99264f1df9
Technus <daniel112092@gmail.com>
2013-10-23 20:13:19 +0200

    Update mainframe main.lua

--- mainframe main.lua
+++ mainframe main.lua
@@ -51,94 +51,80 @@ function getindex(tablename,data,case,row)--search for index --yes row not colum
   end
   return false
 end
+
+
 --List of Fuctions for redstone interactions
 
 --adds ON-OFF redstone (analog) and togglable by impulse redstone flipflop, I/O's to database
-function addIO(ID,name,descr,method,trough,troughside,side,color,dir,remote,negated)
-  --1-id - ID in database (on already used line it will overwrite)
-  --2-name - short name of the node
-  --3-descr -longer description of node
-  --4-method ,0-redIObool 1-redIOm(impulsed flipflop) 2-redIOanalog 3-MFRbool 4-MFRm 5-MFRanalog
-  --5-trough if not = 0 -> send trough pc (value is pc id)  [if non advanced pc - use negative number? ]
-  --6-troughside - side of the passtrough pc that is used 
-  ---MOVED memorized - (moved to method)
-  --7-side - to select which side to use for MFRcontroller
-  --8-color - to select which side to use for MFRcontroller
-  --9-dir - direction 0 is output 1 is input
+function addIO(ID,name,descr,method,trough,troughside,side,color,dir,remote,negated,state)
+  
+  --works like modIO for existing ID's
+  
+  -- 1-id - ID in database (on already used line it will overwrite)
+  -- 2-name - short name of the node
+  -- 3-descr -longer description of node
+  -- 4-method ,0-redIObool 1-redIOm(impulsed flipflop) 2-redIOanalog 3-MFRbool 4-MFRm 5-MFRanalog
+  -- 5-trough if not = 0 -> send trough pc (value is pc id)  [if non advanced pc - use negative number? ]
+  -- 6-troughside - side of the passtrough pc that is used 
+  -- 7-side - to select which side to use for MFRcontroller
+  -- 8-color - to select which side to use for MFRcontroller
+  -- 9-dir - direction 0 is output 1 is input
   --10-remote - allows mainframe to change the state if = 0
-  ---remote - if = 1 mainframe can only read (other pc is changing that state)
-  --11-negated - 1 ifthe output is negated - allows keeping track of it (will show negated output ie 
-  ---negated - (function returns 1 to check ifit is on but the monitor shows 0)
-  local ID=math.ceil(ID)--makes sure that it is integer
-  if ID<1 or ID==nil then ID=#rDB+1 end  --if ID not given 
+  --  -remote - if = 1 mainframe can only read (other pc is changing that state)
+  --11-negated - 1 if the output is negated - allows keeping track of it (will show negated output ie 
+  --  -negated - (function returns 1 to check if it is on but the monitor shows 0)
+  
+  --12-state - desired state is NOT necessary (defaults to 0)
+  
+  local oldlenght=#rDB
+  if ID==nil then--auto find Free ID
+    ID=1
+    local i=1
+    while i<=oldlenght do
+      if ID<rDB[i][1] then ID=rDB[i][1] end
+        i=i+1
+    end
+ 
+  end  --conditions
+  if state==nil then state=0 end
   local target=getindex(rDB,ID,0,1)--looks for ID
-  if target then else target=#rDB+1 end--not found? meh make new entry
-  table.insert(rDB,target,{ID,name,descr,method,trough,troughside,side,color,dir,remote,negated,0})--inserts new blank entry on 1st free index storing there the ID
+  if target then table.remove(rDB,target) else target=#rDB+1 end--not found? meh make new entry
+  table.insert(rDB,target,{ID,name,descr,method,trough,troughside,side,color,dir,remote,negated,state})--inserts new record
   save(rDB,"redstoneDB")
   if oldlenght==#rDB then return(true) else return(false) end--returns true if overwritten something
 end
 
-function rmIO(data,idaddrname)--removing redstone IO node
-  --data specify whitch node to delete
-  --idaddrname - 0- use ID    1-use table INDEX 2- name exact 3-not case sensitive
-  --name = 1 then use name of the node instead ID
-  --name = 0 then use the file line from redstoneDB file
-  local oldlenght=#rDB--lenght save
-  end
-  if idaddrname==0 then--if look by ID
-    local target=1
-    while target<=#rDB do
-      if rDB[target][1]==data then
-        table.remove(rDB,target)
-        break
-      else
-        target=target+1
-      end
-    end
-  
-  elseif idaddrname==1 then--if look by INDEX
-    table.remove(rDB,data)
-    
-  elseif idaddrname==2 then--if look by name--case sensitive
-    local target=1
-    while target<=#rDB do--case sensitive
-      if rDB[target][2]==data then
-        table.remove(rDB,target)
-        break
-      else
-        local target=target+1
-      end
-    end
-  elseif idaddrname==3 then--if look by name--NOT case sensitive
-    local data=string.lower(data)--changes to lowercase AFTER the non lower case loop
-    local target=1
-    while target<=#rDB do--not case sensitive
-      if string.lower(rDB[target][2])==data then
-        table.remove(rDB,target)
-        break
-      else
-        local target=target+1
-      end
-    end
-  end
-  save(rDB,"redstoneDB")
-  if oldlenght==#rDB then return(false) else return(true) end--returns true if overwritten something
+function modIO(index,name,descr,method,trough,troughside,side,color,dir,remote,negated,ID)-- ID change optional
+  rDB[index][2]=name
+  rDB[index][3]=descr
+  rDB[index][4]=method
+  rDB[index][5]=trough
+  rDB[index][6]=troughside
+  rDB[index][7]=side
+  rDB[index][8]=color
+  rDB[index][9]=dir
+  rDB[index][10]=remote
+  rDB[index][11]=negated
+  if ID~=nil then rDB[index][1]=ID end
 end
 
+function rmIO(index)--removes entry from rDB
+  table.remove(rDB,index)
+end
 
-function readIO(data,idaddrname)--reading IO node value ! real value
+function readIO(index)--reading IO node value ! real value
   
 end
 
-function freadIO(data,idaddrname)--reading IO node value ! stored in files
+function freadIO(index)--reading IO node value ! stored in files
   
 end
 
-function setIO(data,idaddrname,value)--setting IO node value to persistence and real circuit
+function setIO(index,value)--setting IO node value to persistence and real circuit
   
 end
 
-function initIO(data,idaddrname)--sets IO from persistence,works like setIO but no data is used
+function initIO(index)--sets IO from persistence,works like setIO but no data is used
   --freadIO->setIO
   setIO(data,idaddrname,freadIO(data,idaddrname))
 end
@@ -167,7 +153,10 @@ if fs.exists("redstoneDB") then --checker for file
 else
   rDB = fs.open("redstoneDB","r")
   rDB.close()
-  --rDBnew()--function to make new file contents
+  
+  --ID is always connected to one certain task - if you move a machine you MUST use the same id to not change every other pc
+  
+  
   --[[database memory map
   OLD--[INDEX of tables inside] rDB ->[INDEX is in every one] ID,name,descr,method,trough,troughside,side,color,dir,remote,negated,(actual/desired status)
   OLD--so rDB is array of 12 tables which store all RS interaction info
@@ -187,8 +176,12 @@ else
   ---remote - if = 1 mainframe can only read (other pc is changing that state)
   --11-negated - 1 ifthe output is negated - allows keeping track of it (will show negated output ie 
   ---negated - (function returns 1 to check ifit is on but the monitor shows 0)
-  --12-desired state
+  --12-state - desired state - of the output !ignore negated when thinking about it it is connected to command not the functional state
   
+  negated=1    computer(state)->functoinalblock(state)->negating torch(!negated state,so  not just state)->output(not State)
+  negated=0    computer(state)->functoinalblock(state)->output(state)
+  state - always stores things that should be before negating torch 
+  negated - stores info if the signal is used as negated or not
   --]]
 end
 if fs.exists("config") then --checker for file
