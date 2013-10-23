a2858c5dfa6fb29a9b1baf0b9e7c71dc5d2c88ab
Technus <daniel112092@gmail.com>
2013-10-23 18:52:03 +0200

    Good GetINdeX

--- mainframe main.lua
+++ mainframe main.lua
@@ -14,17 +14,46 @@ function load(name)--loads table from file
   return textutils.unserialize(data)--returns contents
 end
 --table search
-function getindex(tablename,data,row)--search for index --yes row not colum (rotated tables)
+function getindex(tablename,data,case,row)--search for index --yes row not colum (rotated tables)
+  --tablename - table name
+  --data what to look for
+  --0- SENSItiVe-used for all , 1-NOT case sensitive-strings ONLY, 2-NOT number sign sensitive-numbers ONLY
+  --row specifies in which value of DIM2 of table to look for
   local target=1
-  while target<=#tablename do
-    if rDB[target][row]==data then
-      return(target)
-      break
-    else
-      target=target+1
+  local size=#tablename
+  if case==0 then
+    while target<=size do
+      if rDB[target][row]==data then
+        return(target)
+        break
+      else
+        target=target+1
+      end
+    end
+  elseif case==1 then
+    data=string.lower(data)
+    while target<=size do
+      if string.lower(rDB[target][row])==data then
+        return(target)
+        break
+      else
+        target=target+1
+      end
+    end
+  
+  elseif case==2 then
+    data=math.abs(data)
+    while target<=size do
+      if math.abs(rDB[target][row])==data then
+        return(target)
+        break
+      else
+        target=target+1
+      end
     end
   end
-  return 0
+
+  return false
 end
 --List of Fuctions for redstone interactions
 
@@ -91,7 +120,7 @@ function rmIO(data,idaddrname)--removing redstone IO node
         local target=target+1
       end
     end
-  else if idaddrname==3 then--if look by name--NOT case sensitive
+  elseif idaddrname==3 then--if look by name--NOT case sensitive
     local data=string.lower(data)--changes to lowercase AFTER the non lower case loop
     local target=1
     while target<=#rDB do--not case sensitive
@@ -107,16 +136,13 @@ function rmIO(data,idaddrname)--removing redstone IO node
   if oldlenght==#rDB then return(false) else return(true) end--returns true if overwritten something
 end
 
-function getindex(data,idnme)--search for index
-  
-end
 
 function readIO(data,idaddrname)--reading IO node value ! real value
   
 end
 
 function freadIO(data,idaddrname)--reading IO node value ! stored in files
-
+  
 end
 
 function setIO(data,idaddrname,value)--setting IO node value to persistence and real circuit
