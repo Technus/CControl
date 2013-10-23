1715351e2b27593a60fc5b39502017b2181591bd
Technus <daniel112092@gmail.com>
2013-10-23 16:56:04 +0200

    Update HIDPC.lua

---
+++ HIDPC.lua
@@ -0,0 +1,16 @@
+--[[com protocol
+
+start
+
+send encryption data
+
+table - auth(name,passchecksum,PASSCHECKSUM,timestamp)
+
+incoming - Boolean value 1-auth succesfull 0-ERROR,wrong   OK(0/1)
+
+login OK - cansend the command 
+
+table - command(commandtype,var1,var2,var3,.... and so on)
+
+login bad- lockdown for 10+ 30+ and so on...
+--]]
