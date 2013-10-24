7bca3922ce610d98bc33209a224308549633f077
Technus <daniel112092@gmail.com>
2013-10-24 20:47:12 +0200

    Update Comunication protocols and such mini scrypts.lua

--- Comunication protocols and such mini scrypts.lua
+++ Comunication protocols and such mini scrypts.lua
@@ -11,15 +11,20 @@ command table how to
 auth table- meh....
 commandtype
 LI - login-no variables-will allow that pc to interact with mainframe
-LO - logout-novariables-will disable that pc to interact with mainframe
+LO - logout-novariables-will disable that pc mainframe interactions
 RR - rDBread-var1-ID -reads NODE
-RF - rDBfread-var1-ID reads from rDB file
-RW - rDBwrite-var1-ID-var2-value writes state to cennected NODE
-RF - rDBfwrite-var1-ID-var2-value writes to rDBfile
+RF - rDBfread-var1-ID reads node from rDB FILE
+RI - reads from NODE andupdates rDB
+RW - rDBwrite-var1-ID-var2-value writes state to NODE
+RV - rDBfwrite-var1-ID-var2-value writes to rDB FILE
+RO - wtites to NODE and updates rDB
 RN - rDBnew vartable ->(ID,name,descr,pcID,pcSIDE,method,functorID,functorSIDE,functorCOLOR,negated,state)
     (state is optional, negated is optional - both defaults to 0)
 RM -rDB remove var1-ID
 RG - recieves whole rDB
+
+rr - redstone read-sent from mainframe execution of RR,RI
+rs - redstone set- sent from mainframe execution of RW,RO
 
 UR -var1-name,var2-acceslevel-var3[rDB permission nodes],var4[sDB permission noedes],var[uDB permission nodes],reads user entry
 UW -var1[2DIM table of changes],writes to user entry
