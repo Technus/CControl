a58f98aa6d31683f642d4a305da02289a24e080e
Technus <daniel112092@gmail.com>
2013-10-24 09:03:31 +0200

    Update Comunication protocols and such mini scrypts.lua

--- Comunication protocols and such mini scrypts.lua
+++ Comunication protocols and such mini scrypts.lua
@@ -1,3 +1,29 @@
+--[[com protocol
+
+start
+
+send encryption data
+
+table - auth(name,passchecksum,PASSCHECKSUM,timestamp)
+
+incoming - Boolean value 1-auth succesfull 0-ERROR,wrong   OK(0/1)
+
+login OK - cansend the command 
+
+table - command(authTABLE,commandtype,var1,var2,var3,.... and so on)
+in return Bool confirmation 1-OK 0-OFF
+login bad- lockdown for 10+ 30+ and so on...
+
+
+command table how to
+
+
+
+--]]
+
+
+
+
 --Sending end
 --Midway
 
