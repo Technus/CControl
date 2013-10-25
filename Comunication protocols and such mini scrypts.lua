0558690cc3902cdd7d59a638e1e798d54395ef6e
basdxz <tudurap.com@gmail.com>
2013-10-25 14:08:51 +0100

    A few new fucntions

--- Comunication protocols and such mini scrypts.lua
+++ Comunication protocols and such mini scrypts.lua
@@ -21,7 +21,7 @@ command table how to
 auth table- meh....
 commandtype
 LI - login-no variables-will allow that pc to interact with mainframe
-LO - logout-novariables-will disable that pc mainframe interactions
+LO - logout-no variables-will disable that pc mainframe interactions
 RR - rDBread-var1-ID -reads NODE
 RF - rDBfread-var1-ID reads node from rDB FILE
 RI - reads from NODE andupdates rDB
@@ -54,27 +54,44 @@ UM -var1[userID]removes user
 --auth table
 --[1]userID [2]userNAME [3]checksum [4]CHECKSUM [5]timestamp
 
-
---Sending end
-local fucntion EncPassTime(pass)
+local function encPassTime(pass)
+--Encrypts the password with a timestamp
 local TimeS = toString(1440*os.day()+os.time())
 return {enc.encrypt(pass,TimeS),TimeS}
 end
 
-local fucntion encript(pass,TimeS,data)
-local passT = {pass, TimeS}
+local fucntion LI(UID, user, pass, channel)
+--Tries to login into the server
+local passTE, TimeS = EncPassTime(pass)
+local authT = {UID, user, passTE, TimeS}
+local msgS = textutils.serialize({"LI",authT})
+rednet.send(sendC,msgS)
+--Recives and proccesses the messsage?
+end
+
+local fucntion LO(SID , channel)
+--Tries to logout of the server
+local passTE, TimeS = EncPassTime(pass)
+local authT = {UID, user, passTE, TimeS}
+local msgS = textutils.serialize({"LO",authT})
+modem.transmit(sendC,receiveC,msgS)
+--Recives and proccesses the messsage?
+end
+
+local function encrypt(pass,data)
+--Encrypts the message with the time stamp and password
+local passTE, TimeS = EncPassTime(pass)
+local passT = {passTE, TimeS}
 local passS = textutils.serialize(passT)
 local dataE = enc.encript(data, passT)
+return dataE
 end
---Midway
 
--- Turn on the modems!
+function rednetOn()
 sides = rs.getSides()
 for i = 1,sides# do
 if "modem" = peripheral.getType(sides[i]) then
 rednet.open(i)
 end
 end
-
-
---Reciving end
+end
