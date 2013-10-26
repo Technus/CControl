9496ac29a9ccaf48b378701ddfd1c67fb95909ba
Technus <daniel112092@gmail.com>
2013-10-26 20:22:24 +0200

    Update communication table.lua

--- COMMON/communication table.lua
+++ COMMON/communication table.lua
@@ -2,7 +2,7 @@
 if not COM then os.loadAPI"COM")end
 local comtable = {
 --LI - login
-{XXnum("LI"),"Sending Login Command", "Recieved Login command", --[[placeholder for function]]},
+{COM.XXnum("LI"),"Sending Login Command", "Recieved Login command", --[[placeholder for function]]},
 --LO - logout
-{XXnum("LO"),"Sending Logout Command","Recieved Logout command",--[[placeholder for function]]},
+{COM.XXnum("LO"),"Sending Logout Command","Recieved Logout command",--[[placeholder for function]]},
 }
