7209bd0bfadecd5035d46bdd55fcd73b6637e2df
Technus <daniel112092@gmail.com>
2013-10-26 20:19:39 +0200

    Create communication table.lua

---
+++ COMMON/communication table.lua
@@ -0,0 +1,8 @@
+--template to copy paste
+
+local comtable = {
+--LI - login
+{XXnum("LI"),"Sending Login Command", "Recieved Login command", --[[placeholder for function]]},
+--LO - logout
+{XXnum("LO"),"Sending Logout Command","Recieved Logout command",--[[placeholder for function]]},
+}
