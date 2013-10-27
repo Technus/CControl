60b7356fca7215d2c2d1ce2f1c8cede3b49671b9
Technus <daniel112092@gmail.com>
2013-10-27 23:30:21 +0100

    Update mainframe main.lua

--- MAINFRAME/mainframe main.lua
+++ MAINFRAME/mainframe main.lua
@@ -14,6 +14,60 @@ do--Loading all the API's we are going to use here!
   --COM.recieve() -- recieves data
 end
 
+do--commands definitions
+--TYPE
+--L - login
+--U - user
+--P - pc
+--R - redstone
+--O - redstone group
+--D - detector
+--I - detector group
+--M - detector radar
+--C - config
+--ACTIONS
+--  DB - cannot overlap
+--C - check (ex. just check if login went ok, functions that are required to retrurn T/F/nil)
+
+--R - read (you must specify ID, returns one entry)
+--Q - query DB (searches in DB, returns whole entries)
+--G - (global) send whole DB 
+
+--N - (new) creates entry(does not require ID)
+--M - modify entry (selection by ID)
+--T - (trash)removes entry
+--E - ERASES DATABASE :O (superuser only)
+-- uDB
+--D - degrade user ( 0 perms)
+--  rDB
+--I - (in)reads state from rDB
+--O - (out)writes to rDB state
+--P - (phisical)gets the signal from physical node
+--S - sets physical node
+--V - (value) reads the value from table node and writes it to table
+--W - writes rDB and Node state
+-- drDB
+--S - scan area gives players table
+--P - scan for player 
+--O - others (all - players)
+--A - all entites
+--N - neutral
+--F - friendly
+--T - (targets) aggresive mobs
+--  dDB
+--R - read data
+--S - set data ?
+--
+
+
+comtable = {
+--LC - login check
+[COM.XXnum("LI")]={nil,"Recieved Login command", --[[placeholder for function]]},
+--LO - logout
+[COM.XXnum("LO")]={"Sending Logout Command","Recieved Logout command",--[[placeholder for function]]},
+}
+end
+
 do--basic functions
   function authCheck(auth)--check if auth correct
     if auth[1] then
