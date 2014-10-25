feaeb0dcee06b45b19e4814ef0691396950c9f31
Technus <daniel112092@gmail.com>
2014-10-25 10:00:13 +0200

    +

--- ANOTHER ATTEMPT/TABLES AND DEFINITIONS.lua
+++ ANOTHER ATTEMPT/TABLES AND DEFINITIONS.lua
@@ -1,4 +1,4 @@
-data									={}
+local data								={}
 	data.config							={}
 	  data.config.safety				={}
 	  data.config.network				={}
@@ -55,26 +55,29 @@ meta									={}
 	  meta.network.paths				={}--list of network connections
 --	  meta.network.packet				={}
 	meta.log							={}
+	  meta.log.log						={}--general purpose
 	  meta.log.network					={}
-		meta.log.network.packets		={}
-		meta.log.network.changes		={}
-	  meta.log.meta						={}
-	    meta.log.meta.commands			={}
-	    meta.log.meta.answers			={}
+		meta.log.network.packet			={}
+		meta.log.network.change			={}
+	  meta.log.data						={}--db changes
 
 functions								={}
 	functions.timestamp=function() return 1000*(24*os.day()+os.time() end--as Integer
-
+	functions.tick=function() return (os.time() * 1000 + 18000)%24000 end
 	
+--USER
 function meta.user.single:new(name)
-  local o = {name=name,lastTimeStamp=functions.timestamp()}
+  local o = {name=name or functions.timestamp(),lastTimeStamp=functions.timestamp()}
   setmetatable(o, self)
   self.__index = self
   self.description=nil
   self.photo=nil
-  self.password=nil--not recommended
-  self.passhashU=nil--upper
-  self.passhashL=nil--lower
+  self.password_u=nil--not recommended
+  self.password_m=nil--not recommended
+  self.passhashUu=nil--upper
+  self.passhashLu=nil--lower
+  self.passhashUm=nil--upper
+  self.passhashLm=nil--lower
   self.group={}--user groups inherited
   self.permission={}--what can do
     self.permission.single={}
@@ -86,9 +89,9 @@ function meta.user.single:new(name)
   self.superuser=false
   return o
 end
-
+--USER GROUP
 function meta.user.group:new(name)
-  local o = {name=name}
+  local o = {name=name or functions.timestamp()}
   setmetatable(o, self)
   self.__index = self
   self.description=nil
@@ -103,15 +106,18 @@ function meta.user.group:new(name)
   self.superuser=false
   return o
 end
-
+--CLIENT
 function meta.client.single:new(name)
-  local o = {name=name,lastTimeStamp=functions.timestamp()}
+  local o = {name=name or functions.timestamp(),lastTimeStamp=functions.timestamp()}
   setmetatable(o, self)
   self.__index = self
   self.description=nil
-  self.password=nil--not recommended
-  self.passhashU=nil--upper
-  self.passhashL=nil--lower
+  self.password_c=nil--not recommended
+  self.password_m=nil--not recommended
+  self.passhashUc=nil--upper
+  self.passhashLc=nil--lower
+  self.passhashUm=nil--upper
+  self.passhashLm=nil--lower
   self.group={}--client groups inherited
   self.permission={}--what can do
     self.permission.single={}
@@ -120,9 +126,9 @@ function meta.client.single:new(name)
   self.networkNic={}--connected NICs
   return o
 end
-
+--CLIENT GROUP
 function meta.client.group:new(name)
-  local o = {name=name}
+  local o = {name=name or functions.timestamp()}
   setmetatable(o, self)
   self.__index = self
   self.description=nil
@@ -133,7 +139,7 @@ function meta.client.group:new(name)
   self.hierarchy={}--hierarchy
   return o
 end
-
+==PERMISSION DEFAULT
 --function meta.permission.default:add(name)
 --  local o = {name=name}
 --  setmetatable(o, self)
@@ -141,9 +147,9 @@ end
 --  self.level=0--what can do
 --  return o
 --end
-
+--"PERMISSION" STATES
 function meta.permission.state:new(name)
-  local o = {name=name}
+  local o = {name=name or functions.timestamp()}
   setmetatable(o, self)
   self.__index = self
   self.description=nil
@@ -154,9 +160,9 @@ function meta.permission.state:new(name)
   return o
 end
 --data.permission.state.default=meta.permission.state:new("default")
-
+--PERMISSION GROUPS
 function meta.permission.group:new(name)
-  local o = {name=name}
+  local o = {name=name or functions.timestamp()}
   setmetatable(o, self)
   self.__index = self
   self.description=nil
@@ -166,21 +172,21 @@ function meta.permission.group:new(name)
 --self.hierarchy={}--hierarchy
   return o
 end
-
+--PERIPHERAL
 function meta.peripheral.single:new(name)
-  local o = {name=name}
+  local o = {name=name or functions.timestamp()}
   setmetatable(o, self)
   self.__index = self
   self.description=nil
-  self.location=nil--connected NIC instance
+  self.nic=nil--connected NIC instance
   self.definition=nil
---self.state={}--info about state of peripheral?
+  self.state={}--info about state of peripheral?
 --self.permission={} --linking via name/ID + name from definition
   return o
 end
-
+--PERIPHERAL GROUP
 function meta.peripheral.group:new(name)--of the same definition
-  local o = {name=name}
+  local o = {name=name or functions.timestamp()}
   setmetatable(o, self)
   self.__index = self
   self.description=nil
@@ -189,9 +195,9 @@ function meta.peripheral.group:new(name)--of the same definition
 --self.permission={} --linking via name/ID + name from definition
   return o
 end
-
+--PERIPHERAL DEFINITION
 function meta.peripheral.definition:new(name)
-  local o = {name=name}
+  local o = {name=name or functions.timestamp()}
   setmetatable(o, self)
   self.__index = self
   self.description=nil
@@ -200,30 +206,81 @@ function meta.peripheral.definition:new(name)
 --self.permission={} --linking via name/ID + name from definition
   return o
 end
-
+--NETWORK NIC
 function meta.network.nic:new(name)
-  local o = {name=name}
+  local o = {name=name or functions.timestamp()}
   setmetatable(o, self)
   self.__index = self
   self.description=nil
-  self.client=nil
-  self.route=nil--side/name
-  self.type="wired-modem"--router/terminal/factory PC...Etc.
+  self.client=nil--what pc (nil for direct) connections ??
+				 --(the one sending peripheral ctrl msg)??
+  self.network=nil--connected network instance
+  self.location=nil--side/name
+  self.type=nil--router/terminal/factory PC...Etc.
+  self.passtrough=nil--is pass-trough capable?
   self.defnition=nil  --data.peripheral.definition.wired-modem  
---self.permission={} --linking via name/ID + name from definition
   return o
 end
-
+--NETWORK GROUP
 function meta.network.group:new(name)
-  local o = {name=name}
+  local o = {name=name or functions.timestamp()}
   setmetatable(o, self)
   self.__index = self
   self.description=nil
-  self.client=nil
-  self.route=nil--side/name
-  self.type="wired-modem"--router/terminal/factory PC...Etc.
+  self.path={}--table of paths
+  self.type=nil--router/terminal/factory PC...Etc. (nil==direct)
   self.defnition=nil  --data.peripheral.definition.wired-modem  
---self.permission={} --linking via name/ID + name from definition
+  return o
+end
+--NETWORK PATH
+function meta.network.path:new(name)
+  local o = {name=name or functions.timestamp()}
+  setmetatable(o, self)
+  self.__index = self
+  self.description=nil
+  self.hop={}--table containing {nic}
+  return o
+end
+--LOG
+function meta.log.log:new(name)
+  local o = {name=name or functions.timestamp(),day=os.day(),time=os.time(),tick=functions.tick()}
+  setmetatable(o, self)
+  self.__index = self
+  self.description=nil
+  self.content={}--table containing stuff
+  return o
+end
+--LOG network packet
+function meta.log.network.packet:new(name)
+  local o = {name=name or functions.timestamp(),day=os.day(),time=os.time(),tick=functions.tick()}
+  setmetatable(o, self)
+  self.__index = self
+  self.description=nil
+  self.nicSource=nil
+  self.nicDestination=nil
+  self.content={}--table containing stuff
+  return o
+end
+--LOG network changes
+function meta.log.network.packet:new(name)
+  local o = {name=name or functions.timestamp(),day=os.day(),time=os.time(),tick=functions.tick()}
+  setmetatable(o, self)
+  self.__index = self
+  self.description=nil
+  self.affectedNic=nil
+  self.change=nil
+  self.content={}--table containing stuff
+  return o
+end
+--LOG database commands/changes
+function meta.log.data:new(name)
+  local o = {name=name or functions.timestamp(),day=os.day(),time=os.time(),tick=functions.tick()}
+  setmetatable(o, self)
+  self.__index = self
+  self.description=nil
+  self.command=nil
+  self.permissionTestResult=nil
+  self.result={}--table containing stuff
   return o
 end
 
