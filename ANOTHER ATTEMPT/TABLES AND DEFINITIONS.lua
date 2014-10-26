06cebd7be0ca2a755658731a8600fdea28b96a0b
Technus <daniel112092@gmail.com>
2014-10-26 11:36:16 +0100

    +

--- ANOTHER ATTEMPT/TABLES AND DEFINITIONS.lua
+++ ANOTHER ATTEMPT/TABLES AND DEFINITIONS.lua
@@ -69,27 +69,65 @@ do--load apis
   --AES.decrypt_str(data, key, iv) -- Decrypt a string.
   if not SHA then os.loadAPI("SHA")end
   --SHA.digestStr(string) -- Produce a SHA256 digest of a string. Uses digest() internally.--returns string,tab[1..8]
+  if not BigInt then os.loadAPI("BigInt")end
 end
 
 do--functions
 functions								={}
-	functions.timestamp=function() return 1000*(24*os.day()+os.time()) end--as Integer
-	functions.tick=function() return (os.time() * 1000 + 18000)%24000 end
+	functions.timestamp=	function() return os.day()..math.floor(os.time()*1000) end--as string which can be BigInt
+								
+	functions.timeReadable=	function() 
+							  local timeValue=os.time() 
+							  return ("Day "..os.day().." Hour "..math.floor(timeValue).." Precision "..((timeValue-math.floor(timeValue))*1000).."/1000 Of Hour" end
 	
-	functions.encryptData=function(data,key,iv)--encrypts anything gives a string
-						  return(AES.encrypt_str(textutils.serialize({"Tec :3",data}), key, iv)) end
+	functions.timeGT=		function(packetTime,storedTime)
+							  packetTime=BigInt.toBigInt(packetTime)
+							  storedTime=BigInt.toBigInt(storedTime)
+							  return BigInt.cmp_gt(packetTime,storedTime) end
+							  
+	functions.timeLE=		function(PacketTime)--LE actualtime
+							  packetTime=BigInt.toBigInt(packetTime)
+							  return BigInt.cmp_le(packetTime,BigInt.toBigInt(functions.timestamp)) end
+							  
+	functions.tick=			function() return (os.time() * 1000 + 18000)%24000 end
+								
+	functions.encryptData=	function(data,key,iv)--encrypts anything gives a string
+						      return(AES.encrypt_str(textutils.serialize({"Tec :3",data}), key, iv)) end
 						  
-	functions.decryptData=function(data,key,iv)--decrypts anything gives the data back
-						  local temp=textutils.unserialize(AES.decrypt_str(data, key, iv))
-						  if not pcall(temp[1]~="Tec :3") then return nil end
-						  if not temp[1]~="Tec :3" then return nil end
-						  if temp[1]~="Tec :3" then return nil else return temp[2] end end
+	functions.decryptData=	function(data,key,iv)--decrypts anything gives the data back
+							  local temp=textutils.unserialize(AES.decrypt_str(data, key, iv))
+							  if not pcall(temp[1]) then return nil end
+							  if not temp[1]~="Tec :3" then return nil end
+							  if temp[1]~="Tec :3" then return nil else return temp[2] end 
+							end
 						  
-	functions.openModems=function()
-						  sides =rs.getSides() 
-						  for i = 1,#sides do
-						  	if "modem" = peripheral.getType(sides[i]) then rednet.open(i) end
-						  end end
+	functions.openModems=	function()
+							  for i = 1,#rs.getSides() do
+							    if "modem" = peripheral.getType(rs.getSides()[i]) then rednet.open(i) end
+							  end
+							end
+						  
+	functions.shaDigest=	function(input) --takes any data returns SHA-256 string
+							  return SHA.digestStr(textutils.serialize(input)) end
+							
+	functions.filler64=		function(input) --takes string and expands it to 64 chars
+							  local t = {}
+							  for i = 1, #input do
+								t[i] = input:sub(i, i)
+							  end
+							  local j=0
+							  while #t<64 do
+								j=j+1
+								t[#t+1]=t[j]
+							  end
+							  local output=t[1]
+							  for k=2,64 do
+								output=output..t[k]
+							  end
+							  return output
+							  end
+ 
+						  
 end	
 
 do--USER
@@ -135,6 +173,25 @@ function meta.user.single:delete()
   self.hierarchy=nil--hierarchy
   self.superuser=nil
   self.gone=true
+end
+
+function meta.user.single:edit(what,value)
+  if what== "name" or "description" or "photo" or "superuser" then
+  self[what]=value return true else return false end
+  
+  if what=="password" then
+	if not (self.password_u or self.password_m) then
+	self.password_u=
+    end
+  end
+end
+
+function meta.user.single:setpassword(value_u,value_m)
+  if self.password_u and self.password_m then return false else
+  self.password_u=value_u
+  self.password_m=value_m
+  return true 
+  end
 end
 end
 
